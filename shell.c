43deaf739221a99d02e799523f6f69a273659e59
Nate <ntuckertriplet@gmail.com>
2019-05-29 12:49:26 +0000

    header files

---
+++ shell.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]){
+	
+
+	return 0;
+}
