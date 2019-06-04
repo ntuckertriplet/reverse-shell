be2fc90fd6fd63009011564b1ac7758c62fd3488
Nate Tucker <ntuckertriplet@gmail.com>
2019-06-04 13:06:29 -0500

    init file

---
+++ foo.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+### BEGIN INIT INFO
+# Provides: BINARY NAME HERE
+# Required-Start: $all
+# Required-Stop:
+# Default-Start 2 3 4 5
+# Default-Stop:
+# Short-Description: shell
+### END INIT INFO
+cd /
+./shelled
